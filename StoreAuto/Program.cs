53e9e8811c2bba90bc3d3b232cf3513631acfc43
rad4949 <igor12radchuk@gmail.com>
2022-11-09 19:18:22 +0200

    first commit

---
+++ StoreAuto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StoreAuto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
