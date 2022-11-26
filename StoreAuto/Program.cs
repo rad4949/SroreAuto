e1c22f13c495a227d25eda60d9262bd466d2c3ad
rad4949 <igor12radchuk@gmail.com>
2022-11-26 17:59:03 +0200

    Added examples of using union, except, intersect, join, distinct, group by, aggregate functions

--- StoreAuto/Program.cs
+++ StoreAuto/Program.cs
@@ -14,18 +14,45 @@ namespace StoreAuto
         {
             DefaultDatabase();
 
-            Console.WriteLine("------------------------Read-------------------------------");
+            Console.WriteLine("\n------------------------Read-------------------------------");
             Read_LINQ_Method();
             Create();
-            Console.WriteLine("------------------------Create-------------------------------");
+            Console.WriteLine("\n------------------------Create-------------------------------");
             Read_LINQ_Query_Syntax();
             Update();
-            Console.WriteLine("------------------------Update-------------------------------");
+            Console.WriteLine("\n------------------------Update-------------------------------");
             Read_LINQ_Query_Syntax();
             Delete();
-            Console.WriteLine("------------------------Delete-------------------------------");
+            Console.WriteLine("\n------------------------Delete-------------------------------");
             Read_Select_Many();
 
+
+            Console.WriteLine("\n------------------------Union-------------------------------");
+            Union();
+            Console.WriteLine("\n------------------------Except-------------------------------");
+            Except();
+            Console.WriteLine("\n------------------------Intersect-------------------------------");
+            Intersect();
+            Console.WriteLine("\n------------------------Join-------------------------------");
+            Join();
+            Console.WriteLine("\n------------------------GroupBy-------------------------------");
+            GroupBy();
+            Console.WriteLine("\n------------------------DistinctGroupBy-------------------------------");
+            DistinctGroupBy();
+            Console.WriteLine("\n------------------------Any-------------------------------");
+            Any();
+            Console.WriteLine("\n------------------------All-------------------------------");
+            All();
+            Console.WriteLine("\n------------------------Min-------------------------------");
+            Min();
+            Console.WriteLine("\n------------------------Max-------------------------------");
+            Max();
+            Console.WriteLine("\n------------------------Average-------------------------------");
+            Average();
+            Console.WriteLine("\n------------------------Sum-------------------------------");
+            Sum();
+            Console.WriteLine("\n------------------------Count-------------------------------");
+            Count();
         }
 
         public static void DefaultDatabase()
@@ -223,5 +250,202 @@ namespace StoreAuto
             context.Clients.Remove(temp);
             context.SaveChanges();
         }
+
+        public static void Union()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+
+            var query = context.Cars.Select(x => x.CompleteSetId)
+                .Union(context.Cars.Include(x => x.CompleteSet)
+                .Where(x => x.CompleteSet.Order != null).Select(x => x.Id));
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"CompleteSetId: {item}");
+            }
+        }
+
+        public static void Except()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var selector1 = context.Cars.ToList();
+            var selector2 = context.Cars.Include(x => x.CompleteSet)
+                .Where(x => x.CompleteSet.Order != null).ToList();
+
+            var query = selector1.Except(selector2);
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"CompleteSetId: {item.CompleteSetId}");
+            }
+        }
+
+        public static void Intersect()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var query = context.Cars.Select(x => x.CompleteSetId)
+                .Intersect(context.Cars.Include(x => x.CompleteSet)
+                .Where(x => x.CompleteSet.Order != null).Select(x => x.Id));
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"CompleteSetId: {item}");
+            }
+        }
+
+        public static void Join()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var query = context.Clients
+               .Join(
+               context.Invoices,
+               cl => cl.Id,
+               i => i.ClientId,
+               (cl, i) => new { Client = cl, Invoice = i })
+               .Join(
+               context.Cars,
+               ClI => ClI.Invoice.CarId,
+               car => car.Id,
+               (ClI, car) => new { ClI.Invoice, ClI.Client, Car = car })
+               .Join(
+               context.CompleteSets,
+               ClIC => ClIC.Car.CompleteSetId,
+               com => com.Id,
+               (ClIC, com) => new { ClIC.Invoice, ClIC.Client, ClIC.Car, CompleteSet = com })
+               .Join(
+               context.Models,
+               ClICC => ClICC.CompleteSet.ModelId,
+               mod => mod.Id,
+               (ClICC, mod) => new { ClICC.Invoice, ClICC.Client, ClICC.Car, ClICC.CompleteSet, Model = mod })
+               .Join(
+               context.Brands,
+               ClICCM => ClICCM.Model.BrandId,
+               br => br.Id,
+               (ClICCM, br) => new { ClICCM.Invoice, ClICCM.Client, ClICCM.Car, ClICCM.CompleteSet, ClICCM.Model, Brand = br })
+               .Select(c => new
+               {
+                   c.Client,
+                   c.Invoice,
+                   c.Car,
+                   c.CompleteSet,
+                   c.Model,
+                   c.Brand
+               });
+
+            foreach (var item in query)
+            {
+                Console.WriteLine("----------------------------------------------------------");
+                Console.WriteLine($"Client: {item.Client.FirstName} {item.Client.LastName}");
+                Console.WriteLine($"Invoice: {item.Invoice.Id}");
+                Console.WriteLine($"Car: {item.Brand.BrandName} {item.Model.ModelName} - {item.Brand.Country}");
+                Console.WriteLine($"Complete set: ");
+                Console.WriteLine($"         * Engine volume = {item.CompleteSet.EngineVolume}");
+                Console.WriteLine($"         * Fuel type = {item.CompleteSet.FuelType}");
+                Console.WriteLine($"         * Model year = {item.CompleteSet.ModelYear}");
+                Console.WriteLine($"         * Price = {item.CompleteSet.Price}");
+            }
+        }
+
+        public static void GroupBy()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var groups = context.Cars
+                .GroupBy(x => x.ColorName)
+                .Select(m => new
+                {
+                    m.Key,
+                    Count = m.Count()
+                })
+                .ToList();
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine($"{group.Key} - {group.Count}");
+            }
+        }
+
+        public static void DistinctGroupBy()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var groups = context.Cars
+                .Select(m => m.ColorName)
+                .Distinct()
+                .ToList();
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine($"{group}");
+            }
+        }
+
+        public static void Any()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            bool result = context.Clients.Any(u => u.FirstName == "Igor");
+
+            Console.WriteLine(result);
+        }
+
+        public static void All()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            bool result = context.Clients.All(u => u.FirstName == "Igor");
+
+            Console.WriteLine(result);
+        }
+
+        public static void Min()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var minPrice = context.CompleteSets.Min(u => u.Price);
+
+            Console.WriteLine($"Min price = {minPrice}");
+        }
+
+        public static void Max()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var maxPrice = context.CompleteSets.Max(u => u.Price);
+
+            Console.WriteLine($"Max price = {maxPrice}");
+        }
+
+        public static void Average()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var avgPrice = context.CompleteSets.Average(u => u.Price);
+
+            Console.WriteLine($"Average price = {avgPrice}");
+        }
+
+        public static void Sum()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var sumPrice = context.CompleteSets.Sum(u => u.Price);
+
+            Console.WriteLine($"Sum price = {sumPrice}");
+        }
+
+        public static void Count()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var cars = context.Cars.Count();
+
+            Console.WriteLine($"Count cars = {cars}");
+        }
+
     }
 }
